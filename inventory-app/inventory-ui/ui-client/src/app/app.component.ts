3862df10531681f9af0c4d287d656ab190a815e9
Markus Funke <funkemarkus@me.com>
2021-01-17 18:25:29 +0100

    first steps with Angular

--- inventory-app/inventory-ui/ui-client/src/app/app.component.ts
+++ inventory-app/inventory-ui/ui-client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'inventory-ui';
+
+  books: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.sendGetRequest().subscribe((data: any) => {
+      console.log(data.books);
+      let booksArray = Object.keys(data.books).map(it => data.books[it])
+      console.log(booksArray)
+      this.books = booksArray;
+    })  
+  }
 }
